7ead2f7728d57c3e8510248f33d2ec1141c6fb04
thalkz <roland.lamidieu@outlook.fr>
2015-12-24 15:16:13 +0100

    Rename files

--- app/src/main/java/com/thalkz/everest/JournalFragment.java
+++ app/src/main/java/com/thalkz/everest/JournalFragment.java
@@ -7,16 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * A placeholder fragment containing a simple view.
+ * JournalFragment displays the journal.
+ * All Events are published here.
  */
-public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
+public class JournalFragment extends Fragment {
+
+    public JournalFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_journal, container, false);
     }
 }
