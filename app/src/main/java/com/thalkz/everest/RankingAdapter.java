b9e1ff0219dc9818f25134b5ccebc0c144b7902d
thalkz <roland.lamidieu@outlook.fr>
2015-12-29 02:02:17 +0100

    Improove RankingFragment's look

--- app/src/main/java/com/thalkz/everest/RankingAdapter.java
+++ app/src/main/java/com/thalkz/everest/RankingAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 /**
@@ -74,7 +75,7 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHold
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public CardView cv;
+        public FrameLayout cv;
         public TextView rank;
         public TextView name;
         public TextView vicDef;
@@ -84,7 +85,7 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHold
         public ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
 
-            cv = (CardView) itemLayoutView.findViewById(R.id.player_cv);
+            cv = (FrameLayout) itemLayoutView.findViewById(R.id.player_cv);
             rank = (TextView) itemLayoutView.findViewById(R.id.player_rank);
             name = (TextView) itemLayoutView.findViewById(R.id.player_name);
             vicDef = (TextView) itemLayoutView.findViewById(R.id.player_vic_def);
