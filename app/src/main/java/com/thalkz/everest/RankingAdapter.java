c7360a6ce01212fe17d5026b4c7fb859e78154a5
thalkz <roland.lamidieu@outlook.fr>
2016-01-02 20:12:42 +0100

    Ranking UI gets much better

--- app/src/main/java/com/thalkz/everest/RankingAdapter.java
+++ app/src/main/java/com/thalkz/everest/RankingAdapter.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.amulyakhare.textdrawable.TextDrawable;
@@ -42,29 +44,14 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHold
 
         viewHolder.getAdapterPosition();
 
-        String vicDefText = "V " + itemsData[position].getVictories() + " / D " + itemsData[position].getDefeats();
-        String pointsText = itemsData[position].getPoints() + " Points";
-        Drawable indic;
+        String rankText = Integer.toString(position + 1);
+        String nameText = itemsData[position].getName();
+        String pointsText = itemsData[position].getPoints() + " m";
 
-        if (itemsData[position].getIndic() == -1) {
-            indic = ContextCompat.getDrawable(context, R.drawable.ic_down);
-        } else if (itemsData[position].getIndic() == 1) {
-            indic = ContextCompat.getDrawable(context, R.drawable.ic_up);
-        } else {
-            indic = ContextCompat.getDrawable(context, R.drawable.ic_same_rank);
-        }
 
-        String rank = Integer.toString(position + 1);
-
-        TextDrawable drawable = TextDrawable.builder()
-                .buildRound(rank, R.color.colorPrimary); //This can be changed to a real color using getResources().getColor()
-        viewHolder.rank.setImageDrawable(drawable);
-
-        viewHolder.name.setText(itemsData[position].getName());
-        viewHolder.vicDef.setText(vicDefText);
+        viewHolder.rank.setText(rankText);
+        viewHolder.name.setText(nameText);
         viewHolder.points.setText(pointsText);
-
-        viewHolder.indic.setImageDrawable(indic);
 
         viewHolder.cv.setOnClickListener(new View.OnClickListener() {
 
@@ -91,21 +78,17 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHold
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public FrameLayout cv;
-        public ImageView rank;
+        public TextView rank;
         public TextView name;
-        public TextView vicDef;
         public TextView points;
-        public ImageView indic;
 
         public ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
 
             cv = (FrameLayout) itemLayoutView.findViewById(R.id.player_cv);
-            rank = (ImageView) itemLayoutView.findViewById(R.id.player_rank);
+            rank = (TextView) itemLayoutView.findViewById(R.id.player_rank);
             name = (TextView) itemLayoutView.findViewById(R.id.player_name);
-            vicDef = (TextView) itemLayoutView.findViewById(R.id.player_vic_def);
             points = (TextView) itemLayoutView.findViewById(R.id.player_points);
-            indic = (ImageView) itemLayoutView.findViewById(R.id.player_indic);
         }
     }
 }
